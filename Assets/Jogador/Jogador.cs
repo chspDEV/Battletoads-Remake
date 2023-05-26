57dd5f6f6fe8bebd3c7b9ac62c7f64b31fcb87ca
Carlos Henrique de Souza Pinto <chspcontato@gmail.com>
2023-05-26 06:46:33 -0300

    ajustes

--- Assets/Jogador/Jogador.cs
+++ Assets/Jogador/Jogador.cs
@@ -4,40 +4,58 @@ using UnityEngine;
 
 public class Jogador : MonoBehaviour
 {
-    // Variáveis de movimento
+    // Variaveis
     public float moveSpeed = 5f;
-    private bool isFacingLeft = false; // Flag para verificar se o jogador está virado para a esquerda
+    private bool isFacingLeft = false; //verificar se o jogador esta virado para a esquerda
 
-    // Referência para o sprite do jogador
+    // Instanciando referencia do sprite do jogador
     private SpriteRenderer spriteRenderer;
+    // Instanciando referencia do animator do jogador
+    private Animator animator;
+    public Transform spritePlayer;
 
-    private void Start()
+    void Start()
     {
-        // Obtenha a referência para o SpriteRenderer do jogador
+        // Pegando de fato o sprite do jogador
         spriteRenderer = GetComponent<SpriteRenderer>();
+
+        // Pegando de fato o animator do jogador
+        animator = spritePlayer.GetComponent<Animator>();
     }
 
-    private void Update()
+    void Update()
     {
-        // Obter entrada horizontal (esquerda: -1, direita: 1)
+        // input horizontal (esquerda: -1, direita: 1)
         float moveDirection = Input.GetAxisRaw("Horizontal");
 
-        // Obter entrada vertical (cima: -1, baixo: 1)
+        // input vertical (cima: -1, baixo: 1)
         float moveDirectionV = Input.GetAxisRaw("Vertical");
 
-        // Mover o jogador na horizonatal
+        //Se esta se movendo
+        if(moveDirection != 0 || moveDirectionV != 0)
+        {
+            //Rodar animacao walking
+            animator.SetBool("movendo", true);
+        }
+        else
+        {
+            //Voltar pro idle
+            animator.SetBool("movendo", false);
+        }
+
+        // Mover o jogador 
         transform.Translate(new Vector2(moveDirection * moveSpeed * Time.deltaTime, moveDirectionV * moveSpeed * Time.deltaTime));
 
-        // Verificar a direção do movimento
+        // Verificar a direcao do movimento
         if (moveDirection < 0 && !isFacingLeft)
         {
-            // Inverter o sprite quando o jogador está virado para a esquerda
+            // Inverter o sprite quando o jogador ta virado para a esquerda
             spriteRenderer.flipX = true;
             isFacingLeft = true;
         }
         else if (moveDirection > 0 && isFacingLeft)
         {
-            // Inverter o sprite quando o jogador está virado para a direita
+            // Inverter o sprite quando o jogador ta virado para a direita
             spriteRenderer.flipX = false;
             isFacingLeft = false;
         }
